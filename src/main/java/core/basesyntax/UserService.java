f8f220b6614903a2f6cb8f8be63ff8b757a153c5
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-03 13:16:17 +0200

    getUserScore method has been implemented

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,7 +2,14 @@ package core.basesyntax;
 
 public class UserService {
 
-    public int getUserScore(String[] records, String email) {
-        return 0;
+    public int getUserScore(String[] records, String email) throws UserNotFoundException {
+
+        String userInfo = java.util.Arrays.stream(records).filter(i -> i.contains(email)).toString();
+
+        if (userInfo == null || userInfo.isBlank() || userInfo.isEmpty()) {
+            throw new UserNotFoundException();
+        }
+
+        return Integer.parseInt(userInfo.substring(userInfo.lastIndexOf(":") + 1));
     }
 }
