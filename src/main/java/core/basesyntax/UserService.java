471ce58c943f097719b509c301bc4ecbc1a33125
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-03 18:10:16 +0200

    Redundant empty lines removed

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,16 +5,12 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-
         for (String record: records) {
-
             String[] userInfo = record.split(":");
-
             if (userInfo[0].equals(email)) {
                 return Integer.parseInt(userInfo[1]);
             }
         }
-
         throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
